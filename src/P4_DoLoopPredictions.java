dafa8ace614e5949786009cb565baab55813191b
Joe Dent <jdent@maryville.edu>
2018-04-08 10:09:02 -0500

    Initial commit

---
+++ src/P4_DoLoopPredictions.java
@@ -0,0 +1,50 @@
+/*
+	ISYS 320
+	Name(s):
+	Date: 
+*/
+
+/*
+ How many times do you expect the loop to repeat? Remember that 
+ “zero,” “infinity,” and “unknown” are legal answers.
+ 
+ What do you expect the loop to output?
+ 
+ (a)
+ Repeats:
+ Expected output:
+ (b)
+ Repeats:
+ Expected output:
+ (c)
+ Repeats:
+ Expected output:
+ (d)
+ Repeats:
+ Expected output:
+   
+ */
+
+/*
+ Was your prediction correct?
+ 
+ (a)
+ (b)
+ (c)
+ (d)
+
+*/
+public class P4_DoLoopPredictions {
+
+	public static void main(String[] args) {
+		System.out.println("STARTING LOOP TEST");
+		
+		
+		// copy and paste the loop to test between here, between the println statements
+		
+				
+		System.out.println("ENDING LOOP TEST");
+
+	}
+
+}
---
+++ src/P5_RandomMessageRepeater.java
@@ -0,0 +1,14 @@
+/*
+	ISYS 320
+	Name(s):
+	Date: 
+*/
+
+public class P5_RandomMessageRepeater {
+
+	public static void main(String[] args) {
+		// Call your getRandomMessage method from here
+		// Be sure you do not print out the message in the getRandomMessage method
+	}
+
+}
